c99349ed25305812afd3756123c48f1896657d69
Kyle McDonald <kyjm2010@gmail.com>
2019-10-01 16:14:16 -0700

    Built the program from home. Hello World! prints out followed by Dodge Challenger. SportsCar class instantiated friom main

---
+++ MySecondSolution/MySecondProgram/SportsCar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySecondProgram
+{
+    public class SportsCar
+    {
+        /// <summary>
+        /// a brief summary
+        /// </summary>
+        /// <remarks>some remarks</remarks>
+        public void myMethod()
+        {
+            Console.WriteLine("Dodge Challenger");
+        }
+    }
+}
